1a86eb42d4f21c83cb7983b33209cf545caff54e
cpucko <cgpucko@gmail.com>
2018-02-23 16:55:14 -0500

    started work, yet to add inputs for change amounts or a comparator between targetSum and moneySum

--- src/CountingChange.java
+++ src/CountingChange.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class CountingChange {
+
+    public static voic main(String[] args) {
+        int targetSum;
+        int moneySum;
+        int pennies;
+        int nickels;
+        int dimes;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter a number between 0 and 99: ");
+        targetSum = input.nextInt();
+        if (targetSum > 99 || targetSum < 0){
+            System.out.println("Sorry, this is not between 0 and 99");
+            System.exit(0);
+        }
+
+
+    }
 }
