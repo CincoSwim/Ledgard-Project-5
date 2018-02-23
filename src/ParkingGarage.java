a8c855608fe35ee930d1ab57d62c45524d918bdd
cpucko <cgpucko@gmail.com>
2018-02-23 16:42:52 -0500

    Calculates amount paid for 5 cars in a parking garage

--- src/ParkingGarage.java
+++ src/ParkingGarage.java
@@ -1,9 +1,44 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.*;
+import java.text.DecimalFormat;
+import java.lang.Math;
 
 public class ParkingGarage {
-    public static void main(String[] args){
-        int HoursParked;
-        int
-    }
+    public static void main(String[] args) {
+        double hoursParked, chargedAmt;
+        double chargeRunningTotal, hoursRunningTotal;
 
+        String pattern, chargeFormatted;
+
+        pattern = "$###,##0.00";
+
+        Scanner input = new Scanner(System.in);
+        DecimalFormat chargeFormat = new DecimalFormat(pattern);
+
+        hoursRunningTotal=0;
+        chargeRunningTotal=0;
+
+        for (int i = 1; i < 6; i++) {
+            System.out.print("Enter the hours parked for car " + i + ":   ");
+
+            hoursParked = input.nextDouble();
+            hoursRunningTotal +=hoursParked;
+
+            if (hoursParked <= 2)
+                chargedAmt = 5.00;
+            else if (hoursParked >= 9)
+                chargedAmt = 12.00;
+            else
+                chargedAmt = 5.0 + (Math.ceil(hoursParked) - 2);
+
+            chargeRunningTotal += chargedAmt;
+        }
+            chargeFormatted = chargeFormat.format(chargeRunningTotal);
+
+        System.out.println("Total Hours " + (float)hoursRunningTotal);
+        System.out.println("Total Charge " + chargeFormatted);
+    }
 }
+
+
