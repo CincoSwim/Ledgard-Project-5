422b8ffe0f3d481dbb3093cc7d51aca3d6b08e5e
CincoSwim <cgpucko@gmail.com>
2018-02-28 11:04:37 -0500

    some nice comments on Oldest and ParkingGarage

--- src/ParkingGarage.java
+++ src/ParkingGarage.java
@@ -1,3 +1,12 @@
+/*
+  EECS 1510 Object-Oriented Programming
+  Project 5 Part 2
+  Written by Christopher Pucko
+
+  Asks the user for the time parked for 5 cars in a parking garage. Then, these times are used to calculate the total
+  income for the garage, bearing in mind that the maximum daily charge is $12 and the minimum fee is $5, depending on
+  time spent.
+*/
 import java.util.*;
 import java.text.DecimalFormat;
 import java.lang.Math;
@@ -23,6 +32,7 @@ public class ParkingGarage {
             hoursParked = input.nextDouble();
             hoursRunningTotal +=hoursParked;
 
+            // Comparators help calculate in regards to the garage's maximum fee and flat fee.
             if (hoursParked <= 2)
                 chargedAmt = 5.00;
             else if (hoursParked >= 9)
